e189cc93ff79e54ab8c9de0fa216655ca039e6cb
RicardoJavierFernandez <rickyjf808@gmail.com>
2019-10-03 23:46:17 -0400

    Created dictionaries of the different dates and tenor rates

--- TreasuryRates.py
+++ TreasuryRates.py
@@ -1,6 +1,8 @@
 import csv
 import requests
+import datetime
 import xml.etree.ElementTree as ET
+
 
 def load_page():
 
@@ -39,6 +41,33 @@ def parse_XML(xmlFile):
             '10YEAR': '{http://schemas.microsoft.com/ado/2007/08/dataservices}BC_10YEAR',
             '20YEAR': '{http://schemas.microsoft.com/ado/2007/08/dataservices}BC_20YEAR',
             '30YEAR': '{http://schemas.microsoft.com/ado/2007/08/dataservices}BC_30YEAR'}
+    
+    # Create dictionary with all the data, keys are the keys in the tags dictionary above.
+    rates_data = {}
+    
+    for key in tags.keys():
+        place_holder = {}
+        for rate_id, rate  in enumerate(root.iter(tags[key])):
+            place_holder[rate_id] = rate.text
+        rates_data[key] = place_holder
+
+    # Loop that goes through each day of data and calculates the 2s10s spread or 3mo10yr. We can use this to capture the dates the spread turned negative
+    date_format = '%Y-%m-%dT%H:%M:%S'
+    
+    for key in rates_data['NEW_DATE']:
+        # format date to mm-dd-yyyy
+        date_converted = datetime.datetime.strptime(rates_data['NEW_DATE'][key], date_format)
+        date_reformatted = date_converted.strftime('%m-%d-%Y')
+        
+        # calculate the 2s10s spread
+        spread = float(rates_data['10YEAR'][key]) - float(rates_data['2YEAR'][key])
+
+        # only print out inverted (negative spreads)
+        if spread < 0:
+            print('Curve inversion on:', date_reformatted, '2s10s spread:', str(round(spread, 2)))
+
+
+    # Testing below for output on different methods 
 
     # for child in root.iter():
     #     print(child.tag)
@@ -54,19 +83,6 @@ def parse_XML(xmlFile):
 ##    for key in tags.keys():
 ##        for child in root.iter(tags[key]):
 ##            print(key, child.text)
-
-    # Create two dictionaries to store 1. The dates, and 2. The tenor's rates
-    date = {}
-    ten_year = {}
-
-    for date_id, date_string in enumerate(root.iter('{http://schemas.microsoft.com/ado/2007/08/dataservices}NEW_DATE')):
-        date[date_id] = date_string.text
-
-    for date_id, date_string in enumerate(root.iter('{http://schemas.microsoft.com/ado/2007/08/dataservices}BC_10YEAR')):
-        ten_year[date_id] = date_string.text
-
-    for key in date.keys():
-        print(date[key], ten_year[key])
 
 
 parse_XML('/Users/ricky/Documents/Python/Treasury-Yield-Curve-XML/TreasuryRates.xml')
